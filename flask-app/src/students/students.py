06798b34d3a16e50234ba0fd2d01748e5f104fad
Neeti Desai <desaineeti4@gmail.com>
2023-12-04 15:23:26 -0500

    Finished students.py endpoints

--- flask-app/src/students/students.py
+++ flask-app/src/students/students.py
@@ -27,7 +27,7 @@ def get_student(student_id):
 
 # View all of a student's folders
 @students.route('/students/<student_id>/studentfolders', methods=['GET'])
-def get_folders(student_id):
+def get_specific_student_student_folders(student_id):
     cursor = db.get_db().cursor()
 
     cursor.execute(f'SELECT * FROM StudentFolders WHERE student_id={student_id}')
@@ -47,7 +47,7 @@ def get_folders(student_id):
 
 # Add a student folder
 @students.route('/students/<student_id>/studentfolders', methods=['POST'])
-def add_new_studentfolder(student_id):
+def add_new_student_folder(student_id):
 
     # collecting data from the request object 
     the_data = request.json
@@ -70,7 +70,7 @@ def add_new_studentfolder(student_id):
 
 # View contents of a specific student folder
 @students.route('/students/<student_id>/studentfolders/<folder_name>', methods=['GET'])
-def get_folder_notes(student_id, folder_name):
+def get_notes_in_student_folder(student_id, folder_name):
     cursor = db.get_db().cursor()
 
     cursor.execute(f'SELECT * FROM StudentFolders WHERE student_id={student_id} AND folder_name = {folder_name}')
@@ -87,3 +87,48 @@ def get_folder_notes(student_id, folder_name):
     response = jsonify(json_folder_data)
 
     return response
+
+
+# Add a note to a specific student folder
+@students.route('/students/<student_id>/studentfolders/<folder_name>', methods=['POST'])
+def add_note_to_student_folder(student_id, folder_name):
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    note_id = the_data['note_id']
+
+    # Constructing the query
+    query = f'''UPDATE Notes
+                SET student_folder = {folder_name}
+                WHERE student_id = {student_id} AND note_id = {note_id}'''
+    
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'
+
+# Delete a specific student folder
+@students.route('/students/<student_id>/studentfolders/<folder_name>', methods=['DELETE'])
+def delete_student_folder(student_id, folder_name):
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    # Constructing the query
+    query = f'''DELETE FROM StudentFolders
+                WHERE student_id = {student_id} AND folder_name = {folder_name}'''
+    
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'
