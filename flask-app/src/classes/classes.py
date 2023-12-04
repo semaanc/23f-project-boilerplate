a185707fe49d93378aed77634bbb53dbfc55c7e8
Celine Semaan <semaanceline@hotmail.com>
2023-12-04 11:51:30 -0500

    Added course_id to most queries

--- flask-app/src/classes/classes.py
+++ flask-app/src/classes/classes.py
@@ -105,8 +105,8 @@ def get_class_folder(course_id, class_id, classf_id):
     return response
 
 
-@classes.route('/classes/<class_id>/classfolders/<classf_id>', methods=['POST'])
-def create_class_folder(classf_id):
+@classes.route('/classes/<course_id>/<class_id>/classfolders/<classf_id>', methods=['POST'])
+def create_class_folder(course_id, class_id, classf_id):
     data = request.get_json()
     folder_name = data['folder_name']
     
@@ -118,14 +118,14 @@ def create_class_folder(classf_id):
     response.status_code = 201
     return response
 
-@classes.route('/classes/<class_id>/students', methods=['GET'])
-def view_students_in_class(class_id):
+@classes.route('/classes/<course_id>/<class_id>/students', methods=['GET'])
+def view_students_in_class(course_id, class_id):
     cursor = db.get_db().cursor()
     cursor.execute(
         'SELECT s.* FROM Students s '
         'JOIN Student_Classes sc ON s.student_id = sc.student_id '
-        'WHERE sc.class_id = ?',
-        (class_id,)
+        'WHERE sc.class_id = ? AND sc.course_id',
+        (class_id, course_id)
     )
     row_headers = [x[0] for x in cursor.description]
     students_data = cursor.fetchall()
@@ -140,25 +140,25 @@ def view_students_in_class(class_id):
     return response
 
 
-@classes.route('/classes/<class_id>/students', methods=['POST'])
-def add_student_to_class(class_id):
+@classes.route('/classes/<course_id>/<class_id>/students', methods=['POST'])
+def add_student_to_class(course_id, class_id):
     data = request.json
     student_id = data['student_id']
 
     cursor = db.get_db().cursor()
     cursor.execute(
         'INSERT INTO Student_Classes (student_id, class_id) VALUES (?, ?)',
-        (student_id, class_id)
+        (student_id, course_id, class_id)
     )
     db.get_db().commit()
     return "Success!"
 
-@classes.route('/classes/<class_id>/students/<student_id>', methods=['DELETE'])
-def remove_student_from_class(class_id, student_id):
+@classes.route('/classes/<course_id>/<class_id>/students/<student_id>', methods=['DELETE'])
+def remove_student_from_class(course_id, class_id, student_id):
     cursor = db.get_db().cursor()
     cursor.execute(
         'DELETE FROM Student_Classes WHERE class_id = ? AND student_id = ?',
-        (class_id, student_id)
+        (course_id, class_id, student_id)
     )
     db.get_db().commit()
 
@@ -167,10 +167,10 @@ def remove_student_from_class(class_id, student_id):
     response.mimetype = 'application/json'
     return response
 
-@classes.route('/classes/<class_id>/oh/<ta_id>', methods=['GET'])
-def get_ta_oh(class_id, ta_id):
+@classes.route('/classes/<course_id>/<class_id>/oh/<ta_id>', methods=['GET'])
+def get_ta_oh(course_id, class_id, ta_id):
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT * FROM OfficeHours WHERE class_id = %s AND ta_id = %s', (class_id, ta_id))
+    cursor.execute('SELECT * FROM OfficeHours WHERE course_id = %s AND class_id = %s AND ta_id = %s', (course_id, class_id, ta_id))
     oh_data = cursor.fetchone()
 
     row_headers = [x[0] for x in cursor.description]
@@ -180,8 +180,8 @@ def get_ta_oh(class_id, ta_id):
     the_response.mimetype = 'application/json'
     return the_response
 
-@classes.route('/classes/<class_id>/oh/<ta_id>', methods=['POST'])
-def add_ta_oh(class_id, ta_id):
+@classes.route('/classes/<course_id>/<class_id>/oh/<ta_id>', methods=['POST'])
+def add_ta_oh(course_id, class_id, ta_id):
     request_data = request.get_json()
 
     # Assuming request_data contains 'time', 'date', and 'location'
@@ -190,22 +190,22 @@ def add_ta_oh(class_id, ta_id):
     location = request_data.get('location')
 
     cursor = db.get_db().cursor()
-    cursor.execute('INSERT INTO OfficeHours (ta_id, course_id, class_id, time, date, location) VALUES (%s, (SELECT course_id FROM Classes WHERE class_id = %s), %s, %s, %s, %s)',
-                   (ta_id, class_id, class_id, time, date, location))
+    cursor.execute('INSERT INTO OfficeHours (ta_id, course_id, class_id, time, date, location) VALUES (%s, %s, %s, %s, %s, %s)',
+                   (ta_id, course_id, class_id, class_id, time, date, location))
     db.get_db().commit()
 
     return "Success!"
 
-@classes.route('/classes/<class_id>/oh/<ta_id>', methods=['REMOVE'])
-def remove_ta_oh(class_id, ta_id):
+@classes.route('/classes/<course_id>/<class_id>/oh/<ta_id>', methods=['REMOVE'])
+def remove_ta_oh(course_id, class_id, ta_id):
     cursor = db.get_db().cursor()
-    cursor.execute('DELETE FROM OfficeHours WHERE class_id = %s AND ta_id = %s', (class_id, ta_id))
+    cursor.execute('DELETE FROM OfficeHours WHERE course_id = %s AND class_id = %s AND ta_id = %s', (course_id, class_id, ta_id))
     db.get_db().commit()
 
-@classes.route('classes/<class_id>/classfolders/<classf_id>/notes/pinned', methods=['GET'])
-def view_pinned_note(class_id, classf_id):
+@classes.route('classes/<course_id>/<class_id>/classfolders/<classf_id>/notes/pinned', methods=['GET'])
+def view_pinned_note(course_id, class_id, classf_id):
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT * FROM Notes WHERE class_id = %s AND class_folder = %s AND pinned = TRUE', (class_id, classf_id))
+    cursor.execute('SELECT * FROM Notes WHERE course_id = %s AND class_id = %s AND class_folder = %s AND pinned = TRUE', (course_id, class_id, classf_id))
     row_headers = [x[0] for x in cursor.description]
     notes_data = cursor.fetchall()
 
@@ -219,8 +219,8 @@ def view_pinned_note(class_id, classf_id):
     return the_response
 
 
-@classes.route('classes/<class_id>/classfolders/<classf_id>/notes/pinned', methods=['POST'])
-def add_pinned_note(class_id, classf_id):
+@classes.route('classes/<course_id>/<class_id>/classfolders/<classf_id>/notes/pinned', methods=['POST'])
+def add_pinned_note(course_id, class_id, classf_id):
     data = request.get_json()
 
     student_id = data.get('student_id')
@@ -229,8 +229,8 @@ def add_pinned_note(class_id, classf_id):
     pinned = data.get('pinned', True)
 
     cursor = db.get_db().cursor()
-    cursor.execute('INSERT INTO Notes (student_id, note_content, reported, pinned, class_folder, class_id) VALUES (%s, %s, %s, %s, %s, %s)',
-                   (student_id, note_content, reported, pinned, classf_id, class_id))
+    cursor.execute('INSERT INTO Notes (student_id, note_content, reported, pinned, class_folder, course_id, class_id) VALUES (%s, %s, %s, %s, %s, %s, %s)',
+                   (student_id, note_content, reported, pinned, classf_id, course_id, class_id))
     db.get_db().commit()
 
     response_data = {'Pinned note added successfully'}
